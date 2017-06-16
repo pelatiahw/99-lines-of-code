d03181d77958a540f23dabc76285bb46d764a9c6
InnovationBham_4 <InnovationBham_4@Admins-iMac.local>
2017-06-16 13:42:15 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+/*ar friends = [
+    {
+        friends: 'Kanye'
+        song
+    }, 
+    {
+        friends: 'Kanye'
+    }, 
+    {
+        friends: 'Kanye'
+    },
+    
+]
+
+function displayPosition(friendsArray) {
+    for (var i = 0; i < friendsArray.length; i++) {
+    console.log('+' + friendsArray[i].friends + ' at ' + friendsArray[i]. + '--' + 
+    friendsArray[i].);
+}*/
+
